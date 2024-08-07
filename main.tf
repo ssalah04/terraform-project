b265828fdc4b9c67afd5305a7aa05fcded9c30d9
Sahra <sahra-2000@hotmail.co.uk>
2024-08-07 16:17:17 +0100

    ECR add

--- main.tf
+++ main.tf
@@ -20,3 +20,11 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_ecr_repository" "sd_ecr_repo" {
+  name                 = "sd_ecr_repo"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
